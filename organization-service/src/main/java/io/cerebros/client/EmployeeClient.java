94f4415cc075dab0af7cb734c3d6d9185667faf7
Meir Gutnik <meir@cerebros.io>
2019-07-16 16:42:22 +0300

    remove eureka

---
+++ organization-service/src/main/java/io/cerebros/client/EmployeeClient.java
@@ -0,0 +1,17 @@
+package io.cerebros.client;
+
+import java.util.List;
+
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+import io.cerebros.model.Employee;
+
+@FeignClient(url = "http://employee-service", name = "employee-service")
+public interface EmployeeClient {
+
+	@GetMapping("/organization/{organizationId}")
+	List<Employee> findByOrganization(@PathVariable("organizationId") Long organizationId);
+	
+}
