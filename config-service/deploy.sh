7a3ee506308a2eabc0c7875ea34c507f7b7f0e35
Meir Gutnik <meir@cerebros.io>
2019-07-23 21:28:37 +0300

    add version as param capability

--- config-service/deploy.sh
+++ config-service/deploy.sh
@@ -2,14 +2,18 @@
 
 echo
 echo    "################################"
-echo    "Handle config-service..."
-echo    "Cleanup resources..."
+echo    "Deploy of config-service version $1 ..."
+echo    "Cleanup k8s resources..."
 kubectl delete service,deployment config-service
-echo    "Cleanup resources DONE"
-mvn     clean install
-docker  build -t cerebrosio/config-service:1.1 .
-docker  push cerebrosio/config-service:1.1
+echo    "Cleanup k8s resources DONE!"
+mvn     clean install -Dprj.version=$1
+docker  build --build-arg APP_VER=$1 -t cerebrosio/config-service:$1 .
+docker  push cerebrosio/config-service:$1
+sed     -i "s/{APP_VER}/$1/g" deployment.yaml
+echo    "Install k8s resources:"
+cat     deployment.yaml
 kubectl apply -f deployment.yaml
+sed     -i "s/$1/{APP_VER}/g" deployment.yaml
 echo    "Handle config-service DONE"
 echo    "################################"
-echo `date`
+echo    `date`
